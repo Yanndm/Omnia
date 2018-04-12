7b1ceb1343ba2b86f23cb90474129829f57daa7c
Yanndm <yanndelm@outlook.com>
2018-04-12 10:53:09 +0200

    Ajout de la version 2

--- js/main.js
+++ js/main.js
@@ -1,8 +1,28 @@
-var loading = document.querySelector('.loading');
-var body = document.querySelector('body');
+// var loading = document.querySelector('.loading');
+// var body = document.querySelector('body');
+//
+// window.addEventListener('load', function() {
+//   setTimeout(function () {
+//       loading.style.display = 'none';
+//   }, 3500);
+// });
 
-window.addEventListener('load', function() {
-  setTimeout(function () {
-      loading.style.display = 'none';
-  }, 3500);
+var presentationLink = document.querySelector('.presentationLink');
+var persoLink = document.querySelector('.persoLink');
+var pres = document.querySelector('.pres');
+var perso = document.querySelector('.perso');
+
+window.addEventListener('scroll', function() {
+  if (Math.round(window.pageYOffset) >= 5550) {
+    presentationLink.classList.remove("is-active");
+    persoLink.classList.add("is-active");
+    pres.classList.remove("isActiveColor");
+    perso.classList.add("isActiveColor");
+  }
+  else if (Math.round(window.pageYOffset) <= 5550) {
+    presentationLink.classList.add("is-active");
+    persoLink.classList.remove("is-active");
+    perso.classList.remove("isActiveColor");
+    pres.classList.add("isActiveColor");
+  }
 });
